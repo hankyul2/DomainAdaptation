6cfaf61fad284d50a6ce0eaef757b6146c62fcde
hankyul <consistant1y@ajou.ac.kr>
2021-08-28 16:04:54 +0900

    feat: Apply Log functions
    - Result.csv will be created.
    - you do not need to re-define whole train loop

--- src/ModelWrapper.py
+++ src/ModelWrapper.py
@@ -1,31 +1,29 @@
 import time
 import datetime
 import logging
-from pathlib import Path
 
 import torch
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 
+from src.log import setup_directory
+from src.utils import AverageMeter, ProgressMeter, accuracy
+
 
 class BaseModelWrapper:
     def __init__(self, log_name):
         self.best_acc = 0
+        self.best_epoch = 0
         self.model = None
         self.device = None
         self.logger = logging.getLogger()
-        self.log_name = '{}-{}'.format(datetime.datetime.now(), log_name)
-        self.log_best_weight_path = 'log_best_weight/{}'.format(self.log_name)
-        self.writer = SummaryWriter(log_dir='log_tensor_board/{}'.format(self.log_name), filename_suffix=log_name)
-
-        self.setup_directory()
-        self.setup_file_logger('log_text/{}-{}'.format(datetime.datetime.now(), log_name))
-
-
-    def setup_directory(self):
-        Path('log_text').mkdir(exist_ok=True, parents=True)
-        Path('log_tensor_board').mkdir(exist_ok=True, parents=True)
-        Path('log_best_weight').mkdir(exist_ok=True, parents=True)
+        setup_directory(log_name)
+        self.time = datetime.datetime.now().strftime('%Y-%m-%d/%H-%M-%S')
+        self.log_name = '{}/{}'.format(log_name, self.time)
+        self.log_best_weight_path = 'log/best_weight/{}.pth'.format(self.log_name)
+        self.writer = SummaryWriter(log_dir='log/tensor_board/{}'.format(self.log_name),
+                                    filename_suffix=log_name.replace('/', '_'))
+        self.setup_file_logger('log/text/{}.txt'.format(self.log_name))
 
     def setup_file_logger(self, log_file):
         hdlr = logging.FileHandler(log_file)
@@ -44,44 +42,98 @@ class BaseModelWrapper:
         self.writer.add_scalar('Accuracy/train', train_acc, epoch)
         self.writer.add_scalar('Accuracy/test', valid_acc, epoch)
 
-    def save_best_weight(self, model, top1_acc):
+    def save_best_weight(self, model, top1_acc, epoch):
         if top1_acc > self.best_acc:
             self.best_acc = top1_acc
-            self.log('saving best model({:07.4f}%) weight to {}'.format(top1_acc*100, self.log_best_weight_path))
-            torch.save({'weight':model.state_dict(), 'top1_acc':top1_acc}, self.log_best_weight_path)
+            self.best_epoch = epoch
+            self.log('Saving best model({:07.4f}%) weight to {}'.format(top1_acc, self.log_best_weight_path))
+            torch.save({'weight': model.state_dict(), 'top1_acc': top1_acc}, self.log_best_weight_path)
 
     def load_best_weight(self, path=None):
         path = path if path else self.log_best_weight_path
         if self.model is not None and self.device is not None:
             self.model.load_state_dict(torch.load(f=path, map_location=self.device)["weight"])
 
+    def init_progress(self, dl, epoch=None, mode='train'):
+        self.batch_time = AverageMeter('Time', ':6.3f')
+        self.data_time = AverageMeter('Data', ':6.3f')
+        self.losses = AverageMeter('Total Loss', ':7.4f')
+        self.top1 = AverageMeter('Acc@1', ':6.2f')
+        self.top5 = AverageMeter('Acc@5', ':6.2f')
+
+        if mode == 'train':
+            self.progress = ProgressMeter(
+                len(dl),
+                [self.batch_time, self.data_time, self.losses, self.top1, self.top5],
+                prefix="TRAIN: [{}]".format(epoch))
+        elif mode == 'valid':
+            self.progress = ProgressMeter(len(dl), [self.batch_time, self.losses, self.top1, self.top5],
+                                          prefix='VALID: ')
+
+    def forward(self, x, y):
+        std_act, std_feat, std_y_hat = self.model(x)
+        return self.criterion(std_y_hat, y), std_y_hat
+
+    def train(self, train_dl, epoch):
+        debug_step = len(train_dl) // 10
+        self.init_progress(train_dl, epoch=epoch, mode='train')
+        self.model.train()
+
+        end = time.time()
+        for step, (x, y) in enumerate(train_dl):
+            self.data_time.update(time.time() - end)
+
+            x, y = x.to(self.device), y.to(self.device)
+            loss, std_y_hat = self.forward(x, y)
+
+            acc1, acc5 = accuracy(std_y_hat, y, topk=(1, 5))
+            self.losses.update(loss.item(), x.size(0))
+            self.top1.update(acc1[0], x.size(0))
+            self.top5.update(acc5[0], x.size(0))
+
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            self.batch_time.update(time.time() - end)
+            end = time.time()
+
+            if step != 0 and step % debug_step == 0:
+                self.log(self.progress.display(step))
+
+        return self.losses.avg, self.top1.avg
+
     @torch.no_grad()
     def valid(self, dl):
+        debug_step = len(dl) // 10
+        self.init_progress(dl, mode='valid')
         self.model.eval()
-        debug_step, total_step, total_loss, total_acc = 10, len(dl), 0, 0
+
+        end = time.time()
         for step, (x, y) in enumerate(dl):
             x, y = x.float().to(self.device), y.long().to(self.device)
             y_hat = self.model.predict(x)
             loss = F.cross_entropy(y_hat, y)
 
-            _, y_label = y_hat.max(dim=1)
-            acc = (y_label == y).sum().item() / len(y)
+            acc1, acc5 = accuracy(y_hat, y, topk=(1, 5))
+            self.losses.update(loss.item(), x.size(0))
+            self.top1.update(acc1[0], x.size(0))
+            self.top5.update(acc5[0], x.size(0))
 
-            total_loss += loss.clone().detach().item()
-            total_acc += acc
+            self.batch_time.update(time.time() - end)
+            end = time.time()
 
             if step % debug_step == 0:
-                self.log('[valid] STEP: {:03d}/{:03d}  |  loss {:07.4f} acc {:07.4f}%'.
-                         format(step + 1, total_step, loss.clone().detach().item(), acc * 100))
+                self.log(self.progress.display(step))
 
-        return total_loss / total_step, total_acc / total_step
+        return self.losses.avg, self.top1.avg
 
     def fit(self, train_dl, valid_dl, test_dl=None, nepoch=50):
         best_acc = 0
         best_acc_arg = 0
 
         for epoch in range(nepoch):
-            train_loss, train_acc = self.train(train_dl)
+            train_loss, train_acc = self.train(train_dl, epoch)
             valid_loss, valid_acc = self.valid(valid_dl)
 
             if valid_acc > best_acc:
@@ -89,17 +141,17 @@ class BaseModelWrapper:
                 best_acc_arg = epoch + 1
                 if test_dl:
                     self.evaluate(test_dl)
-                self.save_best_weight(self.model, best_acc)
+                self.save_best_weight(self.model, best_acc, best_acc_arg)
 
-            print('=' * 180)
+            print('=' * 150)
             self.log(
-                '[EPOCH]: {:03d}/{:03d}  |  train loss {:07.4f} acc {:07.4f}%  |  valid loss {:07.4f} acc {:07.4f}% ('
+                '[EPOCH]: {:03d}/{:03d}    train loss {:07.4f} acc {:07.4f}%    valid loss {:07.4f} acc {:07.4f}% ('
                 'best accuracy : {:07.4f} @ {:03d})'.format(
-                    epoch + 1, nepoch, train_loss, train_acc * 100, valid_loss, valid_acc * 100, best_acc * 100,
+                    epoch + 1, nepoch, train_loss, train_acc, valid_loss, valid_acc, best_acc,
                     best_acc_arg
                 ))
-            print('=' * 180)
-            self.log_tensorboard(epoch, train_loss, train_acc * 100, valid_loss, valid_acc * 100)
+            print('=' * 150)
+            self.log_tensorboard(epoch, train_loss, train_acc, valid_loss, valid_acc)
 
     @torch.no_grad()
     def evaluate(self, test_dl, ncrop=10):
@@ -115,12 +167,11 @@ class BaseModelWrapper:
             total_y_hat += torch.cat(y_hat, dim=0)
             total_y = torch.cat(total_y, dim=0)
 
-        total_loss = F.cross_entropy(total_y_hat/ncrop, total_y).clone().detach().item()
+        total_loss = F.cross_entropy(total_y_hat / ncrop, total_y).clone().detach().item()
         _, y_label = total_y_hat.max(dim=1)
         total_acc = (y_label == total_y).sum().item() / len(total_y)
 
-        print('=' * 180)
-        self.log('[EVALUATE] {}-crop loss {:07.4f}  |  {}-crop acc {:07.4f}%'.format(ncrop, total_loss, ncrop, total_acc * 100))
-        print('=' * 180)
-
-
+        print('=' * 150)
+        self.log('[EVALUATE] {}-crop loss {:07.4f}  |  {}-crop acc {:07.4f}%'.format(ncrop, total_loss, ncrop,
+                                                                                     total_acc * 100))
+        print('=' * 150)
---
+++ src/log.py
@@ -0,0 +1,164 @@
+import csv
+import datetime
+import os
+import re
+import shutil
+from glob import glob
+from pathlib import Path
+
+from filelock import FileLock
+
+
+class Result:
+    def __init__(self, result_path='log/result.csv', best_result_path='result/best_result'):
+        self.result_path = result_path
+        self.best_result_save_path = best_result_path
+        self.setup()
+
+    @property
+    def no(self):
+        return 0
+
+    @property
+    def acc(self):
+        return 5
+
+    @property
+    def is_best(self):
+        return 10
+
+    @property
+    def weight_path(self):
+        return 11
+
+    def setup(self):
+        Path(self.best_result_save_path).mkdir(exist_ok=True, parents=True)
+        if not Path(self.result_path).exists():
+            self.write_csv(self.result_path, self.get_headers(), mode='w')
+
+    def read_result(self):
+        csv_data = []
+        with open(self.result_path, 'r') as f:
+            for line in csv.reader(f):
+                csv_data.append(line)
+        return csv_data, self.csv2dict(csv_data)
+
+    def csv2dict(self, csv_data):
+        return {col: [row[c] for row in csv_data[1:]] for c, col in enumerate(csv_data[0])}
+
+    def write_csv(self, result_path, data, mode='w'):
+        with open(result_path, mode, newline='') as f:
+            writer = csv.writer(f)
+            for row in data:
+                writer.writerow(row)
+
+    def arg2result(self, args, model):
+        return [self.get_no(), args.model_name, args.src, args.tgt, model.time,
+                model.best_acc.item(), model.best_epoch, args.nepoch, args.lr, args.batch_size, False,
+                model.log_best_weight_path]
+
+    def get_no(self):
+        csv_list, csv_dict = self.read_result()
+        return len(csv_list)
+
+    def check_is_best(self, no):
+        csv_list, csv_dict = self.read_result()
+        return csv_list[no][self.is_best].lower() == 'true'
+
+    def save_result(self, args, model):
+        with FileLock("{}.lock".format(self.result_path)):
+            result = self.arg2result(args, model)
+            self.write_csv(self.result_path, [result], mode='a')
+            best_result, best_idx = self.get_best_result_and_idx(result)
+            self.save_best_result(args, best_idx, best_result)
+
+    def save_best_result(self, args, best_idx, best_result):
+        if len(best_result) == 3:
+            csv_list, csv_dict = self.read_result()
+            for no in best_idx:
+                csv_list[no][self.is_best] = True
+            self.write_csv(self.result_path, csv_list, mode='w')
+            self.write_csv('{}/{}.csv'.format(self.best_result_save_path, get_log_name(args).replace('/', '_')),
+                           self.get_headers() + best_result)
+
+    def get_headers(self):
+        headers = [['no', 'method', 'src', 'tgt', 'start_time', 'acc', 'epoch', 'nepoch',
+                    'lr', 'batch_size', 'is_best', 'model_weight_path']]
+        return headers
+
+    def get_best_result_and_idx(self, result):
+        same_type_result_list = self.get_same_list(result)
+        result_len = len(same_type_result_list)
+        best_result = best_result_idx = []
+        if result_len >= 3:
+            valmax = 0
+            for i in range(result_len - 3):
+                sum_acc = sum([float(row[self.acc]) for row in same_type_result_list[i:i + 3]])
+                if sum_acc > valmax:
+                    valmax = sum_acc
+                    best_result = same_type_result_list[i:i + 3]
+                    best_result_idx = [int(row[self.no]) for row in best_result]
+        return best_result, best_result_idx
+
+    def get_same_list(self, result, start_end=(1, 5)):
+        csv_list, csv_dict = self.read_result()
+        return list(filter(lambda xs: all([xs[i] == result[i] for i in range(*start_end)]), csv_list))
+
+    def get_best_model(self, model_name, pretrained_dataset):
+        result = [0, 'base', pretrained_dataset, model_name]
+        models = max(self.get_same_list(result, start_end=(1, 4)),
+                     key=lambda xs: float(xs[self.acc]))
+        if len(models) < 1:
+            assert Exception('Base model are not prepared yet')
+        return models[self.weight_path]
+
+    def get_best_model_weight_path(self):
+        csv_list, csv_dict = self.read_result()
+        return list(map(lambda x: strip_log_name(x[self.weight_path]), filter(lambda xs: xs[self.is_best].lower() == 'true', csv_list)))
+
+
+def get_base_model(model_name, pretrained_dataset):
+    result_saver = Result()
+    return result_saver.get_best_model(model_name, pretrained_dataset)
+
+
+def get_log_name(args, log_format='{}/{}_{}'):
+    return log_format.format(args.model_name, args.src, args.tgt)
+
+
+def is_best_result_log(log_file_path):
+    log_name = strip_log_name(log_file_path)
+    best_model_path = Result().get_best_model_weight_path()
+    return log_name in best_model_path
+
+
+def strip_log_name(log_file_path):
+    return ''.join(log_file_path.split('.')[0].split('/')[2:])
+
+
+def clear_unused_log(file_path, max_log_len=10):
+    log_file_paths = glob(os.path.join(file_path, '*', '*'))
+    log_file_paths.sort(key=os.path.getmtime)
+    for log_file_path in log_file_paths[:-max_log_len]:
+        keep_log = is_best_result_log(log_file_path)
+        print("It {} is best result log?  {}".format(log_file_path, 'Yes' if keep_log else 'No'))
+        if not keep_log:
+            log_file = Path(log_file_path)
+            if log_file.exists():
+                if log_file.is_dir():
+                    shutil.rmtree(log_file)
+                else:
+                    log_file.unlink(missing_ok=True)
+
+
+def setup_directory(log_name):
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    for prefix in ['text', 'tensor_board', 'best_weight']:
+        Path('log/{}/{}/{}'.format(
+            prefix, log_name, date)
+        ).mkdir(exist_ok=True, parents=True)
+        clear_unused_log('log/{}/{}'.format(prefix, log_name))
+
+
+if __name__ == '__main__':
+    Result()
