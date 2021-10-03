1ff11b73683fbf8296396b5ac646b145c08d1f3b
hankyul <consistant1y@ajou.ac.kr>
2021-10-04 08:46:18 +0900

    chore: Train All model
    - Modify tmuxp yaml session name
    - Add dann.yaml
    - Change main.py to test dynamically

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.cli import MyLightningCLI
 from src.system.base import BaseVisionSystem
 
 if __name__ == '__main__':
-    MyLightningCLI(BaseVisionSystem, LightningDataModule, save_config_overwrite=True,
+    cli = MyLightningCLI(BaseVisionSystem, LightningDataModule, save_config_overwrite=True,
                    subclass_mode_data=True, subclass_mode_model=True)
+    cli.trainer.test(ckpt_path="best")
 
